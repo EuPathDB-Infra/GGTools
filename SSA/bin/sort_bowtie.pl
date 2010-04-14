e35bc76bb0a2253b7f062e0fa796a8d18fcb06be
Greg Grant <ggrant@pcbi.upenn.edu>
2010-04-14 23:11:41 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@34493 735e2a04-f8fc-0310-8a1b-f2942603c481

--- SSA/bin/sort_bowtie.pl
+++ SSA/bin/sort_bowtie.pl
@@ -1,42 +1,60 @@
 #/usr/bin/perl
 
+use strict;
+
 if(@ARGV != 1) {
     die "
-Usage: sort_bowtie.pl <file>
+Usage: sort_bowtie.pl <infile> <outfile> [options]
 
-Where <file> is the name of the file that has been output by Bowtie and the
+Where <infile> is the name of the file that has been output by Bowtie and the
 read names are expected to be of the form seq.Na and seq.Nb where N is a
 positive integer.
+
+Options:
+   -remove : remove the infile if the sort succeeds
 
 This script sorts the rows so that rows with seq.Na and seq.Nb are before
 rows with seq.Ma and seqMb whenever N<M.  Furthermore for fixed N seq.Na is
 before seq.Nb.
 
 ";
-
 }
 
-$filename = $ARGV[0];
-open(INFILE, $filename);
-$tempfilename = $filename . "_temp1." . $chunk;
+my $infile = $ARGV[0];
+my $outfile = $ARGV[1];
+
+my $remove = "false";
+for(my $i=2; $i<@ARGV; $i++) {
+    my $optionrecognized = 0;
+    if($ARGV[$i] eq "-remove") {
+	$remove = "true";
+	$optionrecognized = 1;
+    }
+
+    if($optionrecognized == 0) {
+	die "\nERROR: option '$ARGV[$i]' not recognized\n";
+    }
+}
+
+open(INFILE, $infile);
+my $tempfilename = $infile . "_temp1";
 open(OUTFILE, ">$tempfilename");
-while($line = <INFILE>) {
+while(my $line = <INFILE>) {
     chomp($line);
     $line =~ s/^([^\t]+)\t//;
-    $name = $1;
+    my $name = $1;
     $name =~ s/seq.//;
     $name =~ /(\d+)(a|b)/;
     print OUTFILE "$1\t$2\t$line\n";
 }
 close(OUTFILE);
 close(INFILE);
-$tempfilename2 = $filename . "_temp2." . $chunk;
-$x = `sort -T . -n $tempfilename > $tempfilename2`;
+my $tempfilename2 = $infile . "_temp2";
+my $x = `sort -T . -n $tempfilename > $tempfilename2`;
 $x = `rm $tempfilename`;
 open(INFILE, $tempfilename2);
-$sortedfilename = $type . "_sorted." . $chunk;
-open(OUTFILE, ">$sortedfilename");
-while($line = <INFILE>) {
+open(OUTFILE, ">$outfile");
+while(my $line = <INFILE>) {
     chomp($line);
     $line =~ s/^(\d+)\t(.)//;
     print OUTFILE "seq.$1$2$line\n";
@@ -44,3 +62,12 @@ while($line = <INFILE>) {
 close(OUTFILE);
 close(INFILE);
 $x = `rm $tempfilename2`;
+my $N = -s $infile;
+my  $M = -s $outfile;
+if($N != $M) {
+    die "\nERROR: The sorted file is not the same size as the infile.\n\n";
+}
+
+if($remove eq "true") {
+    $x = `rm $infile`;
+}
