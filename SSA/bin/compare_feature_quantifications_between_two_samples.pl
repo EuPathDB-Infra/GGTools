c6d27785eb1406df544e90b856a4d40bd07fd557
Greg Grant <ggrant@pcbi.upenn.edu>
2010-04-28 12:22:50 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@34712 735e2a04-f8fc-0310-8a1b-f2942603c481

--- SSA/bin/compare_feature_quantifications_between_two_samples.pl
+++ SSA/bin/compare_feature_quantifications_between_two_samples.pl
@@ -2,17 +2,55 @@
 
 # Written by Gregory R. Grant
 # University of Pennslyvania, 2010
+$|=1;
 
-if(@ARGV < 2) {
+if(@ARGV < 3) {
     die "
-Usage: compare_feature_quantifications_between_two_samples.pl <quant_file1> <quant_file2>
+Usage: compare_feature_quantifications_between_two_samples.pl <quant_file1> <quant_file2> <min_depth> [options]
 
-Where <quant_file1> and <quant_file2> are feature quantification files.
+Where:  <quant_file1> and <quant_file2> are feature quantification files
+
+        <min_depth> is the minimum average depth-of-coverage that each gene must have
+        in at least one of the two conditions in order for the gene to be considered.
+
+This program returns (by default) genes ranked by fold change.  Use the options to 
+output at the exon or intron level.
+
+Options: -exons   : rank exons by fold change
+         -introns : rank introns by fold change
+         -annot f : f is a file of gene annotation with col 2 = ucsc id and col 3 = refseq id
+                    col 1 is the annotation that is used in the output.
+
+Note: -exons and -introns can both be set, but if either is set it will not report gene-level
+results, to get genes and exons/introns you have to run twice.
 
 ";
 }
+$min_depth = $ARGV[2];
+$exons="false";
+$introns="false";
+for($i=3; $i<@ARGV; $i++) {
+    $optionrecognized = 0;
+    if($ARGV[$i] eq "-exons") {
+	$exons = "true";
+	$optionrecognized = 1;
+    }
+    if($ARGV[$i] eq "-introns") {
+	$introns = "true";
+	$optionrecognized = 1;
+    }
+    if($ARGV[$i] eq "-annot") {
+	$annotfile = $ARGV[$i+1];
+	$i++;
+	$optionrecognized = 1;
+    }
+    if($optionrecognized == 0) {
+	print STDERR "\nError: option \'$ARGV[$i]\' is not recognized\n\n";
+	exit(0);
+    }
+}
 
-open(INFILE, "UCSC_mouse_knowngene_id_mapping");
+open(INFILE, $annotfile) or die "Error: Cannot open file '$annotfile' for reading.\n\n";
 while($line = <INFILE>) {
     chomp($line);
     @a = split(/\t/,$line);
@@ -26,6 +64,9 @@ $line = <INFILE>;
 $line = <INFILE>;
 $line = <INFILE>; # this is the ----------- ... line
 $genecounter = 0;
+$gene_ave = 0;
+$exon_ave = 0;
+$intron_ave = 0;
 while($line = <INFILE>) { # this is the gene id line
     chomp($line);
     $geneid[$genecounter] = $line;
@@ -36,27 +77,20 @@ while($line = <INFILE>) { # this is the gene id line
     @a = split(/\t/,$line);
     $gene_loc[$genecounter] = $a[1];
     $gene_ave_count[$genecounter] = $a[3];
-    $gene_ave_nrm[$genecounter] = $a[4];
+    $gene_ave_norm[$genecounter] = $a[4];
+    $gene_ave = $gene_ave + $a[4];
     $line = <INFILE>; # this is the first exon line
     chomp($line);
     until(($line =~ /----------/) || !($line =~ /\S/)) {
 	$line =~ s/ //g;
 	@a = split(/\t/,$line);
 	if($a[0] =~ /exon/) {
-	    $a[0] =~ /(\d+)$/;
-	    $exonnumber = $1 - 1;
-	    $exon_loc[$genecounter][$exonnumber] = $a[1];
-	    $exon_ave_count[$genecounter][$exonnumber] = $a[3];
-	    $exon_ave_norm[$genecounter][$exonnumber] = $a[4];
-	    $num_exons[$genecounter]++;
+	    $exon_ave_count{$a[1]} = $a[3];
+	    $exon_ave_norm{$a[1]} = $a[4];
 	}
 	if($a[0] =~ /intron/) {
-	    $a[0] =~ /(\d+)$/;
-	    $intronnumber = $1 - 1;
-	    $intron_loc[$genecounter][$intronnumber] = $a[1];
-	    $intron_ave_count[$genecounter][$intronnumber] = $a[3];
-	    $intron_ave_norm[$genecounter][$intronnumber] = $a[4];
-	    $num_introns[$genecounter]++;
+	    $intron_ave_count{$a[1]} = $a[3];
+	    $intron_ave_norm{$a[1]} = $a[4];
 	}
 	$line = <INFILE>;
 	chomp($line);
@@ -64,12 +98,14 @@ while($line = <INFILE>) { # this is the gene id line
     $genecounter++;
 }
 close(INFILE);
+$gene_ave = $gene_ave / $genecounter;
 
 open(INFILE, $ARGV[1]);
 $line = <INFILE>;
 $line = <INFILE>;
 $line = <INFILE>; # this is the ----------- ... line
 $genecounter = 0;
+$gene_ave2 = 0;
 while($line = <INFILE>) { # this is the gene id line
     chomp($line);
     $geneid2[$genecounter] = $line;
@@ -80,27 +116,20 @@ while($line = <INFILE>) { # this is the gene id line
     @a = split(/\t/,$line);
     $gene_loc2[$genecounter] = $a[1];
     $gene_ave_count2[$genecounter] = $a[3];
-    $gene_ave_nrm2[$genecounter] = $a[4];
+    $gene_ave_norm2[$genecounter] = $a[4];
+    $gene_ave2 = $gene_ave2 + $a[4];
     $line = <INFILE>; # this is the first exon line
     chomp($line);
     until(($line =~ /----------/) || !($line =~ /\S/)) {
 	$line =~ s/ //g;
 	@a = split(/\t/,$line);
 	if($a[0] =~ /exon/) {
-	    $a[0] =~ /(\d+)$/;
-	    $exonnumber = $1 - 1;
-	    $exon_loc2[$genecounter][$exonnumber] = $a[1];
-	    $exon_ave_count2[$genecounter][$exonnumber] = $a[3];
-	    $exon_ave_norm2[$genecounter][$exonnumber] = $a[4];
-	    $num_exons2[$genecounter]++;
+	    $exon_ave_count2{$a[1]} = $a[3];
+	    $exon_ave_norm2{$a[1]} = $a[4];
 	}
 	if($a[0] =~ /intron/) {
-	    $a[0] =~ /(\d+)$/;
-	    $intronnumber = $1 - 1;
-	    $intron_loc2[$genecounter][$intronnumber] = $a[1];
-	    $intron_ave_count2[$genecounter][$intronnumber] = $a[3];
-	    $intron_ave_norm2[$genecounter][$intronnumber] = $a[4];
-	    $num_introns2[$genecounter]++;
+	    $intron_ave_count2{$a[1]} = $a[3];
+	    $intron_ave_norm2{$a[1]} = $a[4];
 	}
 	$line = <INFILE>;
 	chomp($line);
@@ -108,79 +137,71 @@ while($line = <INFILE>) { # this is the gene id line
     $genecounter++;
 }
 close(INFILE);
+$gene_ave2 = $gene_ave2 / $genecounter;
 
-print "GENE_ID\tLocation\tWT_intensity\tMUT_intensity\tratio\n";
-for($i=0; $i<$genecounter; $i++) {
-    if($gene_ave_nrm2[$i] > 0) {
-	$gene_ratio[$i] = $gene_ave_nrm[$i] / $gene_ave_nrm2[$i];
-    }
-    else {
-	$gene_ratio[$i] = 10000;
-    }
-    if($gene_ratio[$i] < 1) {
-	if($gene_ratio[$i] == 0) {
-	    $gene_ratio[$i] = 10000;
-	}
-	else {
+$ratio_adjustment_factor = ($gene_ave + $gene_ave2) / 20;
+
+if($exons eq "false" && $introns eq "false") {
+    print "GENE_ID\tLocation\tintensity1\tintensity2\tadj-ratio\n";
+    for($i=0; $i<$genecounter; $i++) {
+	$gene_ratio[$i] = ($gene_ave_norm[$i]+$ratio_adjustment_factor) / ($gene_ave_norm2[$i]+$ratio_adjustment_factor);
+	if($gene_ratio[$i] < 1) {
 	    $gene_ratio[$i] = 1/$gene_ratio[$i];
 	}
-    }
-    if($gene_ave_count[$i] >= 5 || $gene_ave_count2[$i] >= 5) {
-	$geneid[$i] =~ s/\s+(\+|-)\s*$//;
-	if($geneid[$i] =~ /([^:]*)\(ucscgenes\)/) {
-	    $u = $ucsc{$1};
-	    if($u =~ /\S/) {
-		$name = $u;
+	if($gene_ave_count[$i] >= $min_depth || $gene_ave_count2[$i] >= $min_depth) {
+	    $geneid[$i] =~ s/\s+(\+|-)\s*$//;
+	    if($geneid[$i] =~ /([^:]*)\([^)]*ucsc[^)]*\)/) {
+		$u = $ucsc{$1};
+		if($u =~ /\S/) {
+		    $name = $u;
+		}
+	    }
+	    if($geneid[$i] =~ /([^:]*)\([^)]*refseq[^)]*\)/) {
+		$u = $refseq{$1};
+		if($u =~ /\S/) {
+		    $name = $u;
+		}
+	    }
+	    if($name =~ /\S/) {
+		$gene_hash{"$name\t$gene_loc[$i]\t$gene_ave_norm[$i]\t$gene_ave_norm2[$i]"} = $gene_ratio[$i];
+	    }
+	    else {
+		$gene_hash{"$geneid[$i]\t$gene_loc[$i]\t$gene_ave_norm[$i]\t$gene_ave_norm2[$i]"} = $gene_ratio[$i];
 	    }
 	}
-	if($geneid[$i] =~ /([^:]*)\(refseq\)/) {
-	    $u = $refseq{$1};
-	    if($u =~ /\S/) {
-		$name = $u;
-	    }
-	}
-	if($name =~ /\S/) {
-	    $gene_hash{"$name\t$gene_loc[$i]\t$gene_ave_nrm[$i]\t$gene_ave_nrm2[$i]"} = $gene_ratio[$i];
-	}
-	else {
-	    $gene_hash{"$geneid[$i]\t$gene_loc[$i]\t$gene_ave_nrm[$i]\t$gene_ave_nrm2[$i]"} = $gene_ratio[$i];
-	}
     }
-#    print "$geneid[$i]\t$gene_loc[$i]\t$gene_ratio[$i]\n";
-#    print "geneid[$i] = $geneid[$i]\t$geneid2[$i]\n";
-#    print "gene_loc[$i] = $gene_loc[$i]\t$gene_loc2[$i]\n";
-#    print "gene_ave_count[$i] = $gene_ave_count[$i]\t$gene_ave_count2[$i]\n";
-#    print "gene_ave_nrm[$i] = $gene_ave_nrm[$i]\t$gene_ave_nrm2[$i]\n";
-    for($j=0; $j<$num_exons[$i]; $j++) {
-	if($exon_ave_norm2[$i][$j] > 0) {
-	    $exon_ratio[$i][$j] = $exon_ave_norm[$i][$j] / $exon_ave_norm2[$i][$j];
-	}
-	else {
-	    $exon_ratio[$i][$j] = 10000;
-	}
-	$k = $j+1;
-#	print "exon $k\t$exon_loc[$i][$j]\t$exon_ratio[$i][$j]\n";
-#	print "exon_loc[$i][$j] = $exon_loc[$i][$j]\t$exon_loc2[$i][$j]\n";
-#	print "exon_ave_count[$i][$j] = $exon_ave_count[$i][$j]\t$exon_ave_count2[$i][$j]\n";
-#	print "exon_ave_norm[$i][$j] = $exon_ave_norm[$i][$j]\t$exon_ave_norm2[$i][$j]\n";
-    }
-    for($j=0; $j<$num_introns[$i]; $j++) {
-	if($intron_ave_norm2[$i][$j] > 0) {
-	    $intron_ratio[$i][$j] = $intron_ave_norm[$i][$j] / $intron_ave_norm2[$i][$j];
-	}
-	else {
-	    $gene_ratio[$i] = 10000;
-	}
-	$k = $j+1;
-#	print "intron $k\t$intron_loc[$i][$j]\t$intron_ratio[$i][$j]\n";
-#	print "intron_loc[$i][$j] = $intron_loc[$i][$j]\t$intron_loc2[$i][$j]\n";
-#	print "intron_ave_count[$i][$j] = $intron_ave_count[$i][$j]\t$intron_ave_count2[$i][$j]\n";
-#	print "intron_ave_norm[$i][$j] = $intron_ave_norm[$i][$j]\t$intron_ave_norm2[$i][$j]\n";
+    foreach $key (sort {$gene_hash{$b}<=>$gene_hash{$a}} keys %gene_hash) {
+	$gene_ratio = $gene_hash{$key};
+	print "$key\t$gene_ratio\n";
     }
 }
-#print "-------------------------\n";
+if($exons eq "true") {
+    foreach $exon (keys %exon_ave_norm) {
+	$exon_ratio{$exon} = ($exon_ave_norm{$exon}+$ratio_adjustment_factor) / ($exon_ave_norm2{$exon}+$ratio_adjustment_factor);
+	if($exon_ratio{$exon} < 1) {
+	    $exon_ratio{$exon} = 1/$exon_ratio{$exon};
+	}
+	if($exon_ave_count{$exon} >= $min_depth || $exon_ave_count2{$exon} >= $min_depth) {
+	    $feature_hash{"$exon\tEXON\t$exon_ave_norm{$exon}\t$exon_ave_norm2{$exon}"} = $exon_ratio{$exon};
+	}	
+    }
+}
 
-foreach $key (sort {$gene_hash{$b}<=>$gene_hash{$a}} keys %gene_hash) {
-    $gene_ratio = $gene_hash{$key};
-    print "$key\t$gene_ratio\n";
+if($introns eq "true") {
+    foreach $intron (keys %intron_ave_norm) {
+	$intron_ratio{$intron} = ($intron_ave_norm{$intron}+$ratio_adjustment_factor) / ($intron_ave_norm2{$intron}+$ratio_adjustment_factor);
+	if($intron_ratio{$intron} < 1) {
+	    $intron_ratio{$intron} = 1/$intron_ratio{$intron};
+	}
+	if($intron_ave_count{$intron} >= $min_depth || $intron_ave_count2{$intron} >= $min_depth) {
+	    $feature_hash{"$intron\tINTRON\t$intron_ave_norm{$intron}\t$intron_ave_norm2{$intron}"} = $intron_ratio{$intron};
+	}	
+    }
+}
+if($exons eq "true" || $introns eq "true") {
+    print "Location\ttype\tintensity1\tintensity2\tadj-ratio\n";
+    foreach $key (sort {$feature_hash{$b}<=>$feature_hash{$a}} keys %feature_hash) {
+	$feature_ratio = $feature_hash{$key};
+	print "$key\t$feature_ratio\n";
+    }
 }
