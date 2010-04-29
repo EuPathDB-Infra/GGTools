a4284554d7b9d4ad7a264cce2b9930078a266af2
Greg Grant <ggrant@pcbi.upenn.edu>
2010-04-29 13:38:42 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@34729 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ SSA/bin/excise_regions_from_cov.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+# Written by Gregory R Grant
+# University of Pennsylvania, 2010
+
+if(@ARGV < 2) {
+    die "
+Usage: excise_regions_from_cov.pl <cov file> <regions file> [options]
+
+Where:  <cov file> is a coverage file
+        <regions file> is a file of regions to excise, each row should look like chr:start-end
+
+Options: 
+        -buffer n:  excise also anything within n bases of the regions, default = 0
+
+";
+}
+
+$buffer = 0;
+for($i=2; $i<@ARGV; $i++) {
+    $optionrecognized = 0;
+    if($ARGV[$i] eq "-buffer") {
+	$i++;
+	$buffer = $ARGV[$i];
+	$optionrecognized = 1;
+    }
+
+    if($optionrecognized == 0) {
+	die "\nERROR: option '$ARGV[$i-1] $ARGV[$i]' not recognized\n";
+    }
+}
+
+open(COVFILE, $ARGV[0]);
+open(REGIONS, $ARGV[1]);
+
+$cnt = 0;
+while($region = <REGIONS>) {
+    chomp($regions);
+    $region =~ /^(.*):(\d+)-(\d+)/;
+    $chr[$cnt] = $1;
+    $start[$cnt] = $2;
+    $end[$cnt] = $3;
+    $cnt++;
+}
+close(REGIONS);
+while($line = <COVFILE>) {
+    chomp($line);
+    @a = split(/\t/,$line);
+    $flag = 0;
+    for($i=0; $i<$cnt; $i++) {
+	if($a[1] <= ($end[$i]+$buffer) && $a[2] >= ($start[$i]-$buffer) && $a[0] eq $chr[$i]) {
+	    $flag = 1;
+	}
+    }
+    if($flag == 0) {
+	print "$line\n";
+    }
+}
+close(COVFILE);
