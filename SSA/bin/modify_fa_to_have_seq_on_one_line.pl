7d809bdfd04d80ed6c7f27c52a3e7bdde798e085
Greg Grant <ggrant@pcbi.upenn.edu>
2010-05-10 13:40:51 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@34938 735e2a04-f8fc-0310-8a1b-f2942603c481

--- SSA/bin/modify_fa_to_have_seq_on_one_line.pl
+++ SSA/bin/modify_fa_to_have_seq_on_one_line.pl
@@ -5,14 +5,10 @@
 
 if(@ARGV < 1) {
     die "
-Usage: modify_fa_to_have_seq_on_one_line.pl <fasta_file>
+Usage: modify_fa_to_have_seq_on_one_line.pl <fasta file>
 
-Where <fasta_file> is a fasta file.
-
-This program returns a fasta file that is equivalent to the
-input file, but has the sequences for each entry on one line.
-
-This script outputs to standard out.
+This modifies a fasta file to have the sequence all on one line.  
+Outputs to standard out.
 
 ";
 }
@@ -21,15 +17,15 @@ open(INFILE, $ARGV[0]);
 $flag = 0;
 while($line = <INFILE>) {
     if($line =~ />/) {
-	if($flag == 0) {
-	    print $line;	
-	    $flag = 1;
-	} else {
-	    print "\n$line";
-	}
+        if($flag == 0) {
+            print $line;
+            $flag = 1;
+        } else {
+            print "\n$line";
+        }
     } else {
-	chomp($line);
-	print $line;
+        chomp($line);
+        print $line;
     }
 }
 close(INFILE);
