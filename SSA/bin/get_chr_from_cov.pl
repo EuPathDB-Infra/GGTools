32b9d7073ebd250e3add70436e58b01e19b4a140
Greg Grant <ggrant@pcbi.upenn.edu>
2010-05-16 11:18:45 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@35133 735e2a04-f8fc-0310-8a1b-f2942603c481

--- SSA/bin/get_chr_from_cov.pl
+++ SSA/bin/get_chr_from_cov.pl
@@ -7,11 +7,27 @@ $|=1;
 
 if(@ARGV < 2) {
     die "
-Usage: get_chr_from_cov.pl <cov file> <chr>
+Usage: get_chr_from_cov.pl <cov file> <chr> [options]
 
 Output is to standard out, redirect to a file using \">\"
 
+Options: -name x  : change the name of the track to x. 
+
+If you want to create a file for more than one chromosome, first do each
+separately and give each a different name using -name, and then concatenate
+the files together using:
+> cat file1 file2 .... filen > merged_file
+
 ";
+}
+
+for($i=0; $i<@ARGV; $i++) {
+    $optionrecognized=0;
+    if($ARGV[$i] eq "-name") {
+	$name = $ARGV[$i+1];
+	$i++;
+	$optionrecognized=1;
+    }
 }
 
 open(INFILE, $ARGV[0]);
@@ -19,6 +35,10 @@ open(INFILE, $ARGV[0]);
 # chr9    3342879 3342987 1
 $chr = $ARGV[1];
 $line = <INFILE>;
+if($name =~ /\S/) {
+    $line =~ s/(name=\"[^\"]*\")/name=\"$name\"/;
+    $line =~ s/(description=\"[^\"]*\")/description=\"$name\"/;
+}
 print $line;
 while($line = <INFILE>) {
     if($line =~ /^$chr\t/) {
