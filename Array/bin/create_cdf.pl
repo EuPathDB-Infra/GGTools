fe465f45764c8cdd1392fb28847899af8205eb24
John Brestelli <jbrestell@gmail.com>
2010-08-16 19:13:47 +0000

    use better regex to get x/y coords
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@36434 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Array/bin/create_cdf.pl
+++ Array/bin/create_cdf.pl
@@ -85,13 +85,14 @@ while($line = <INFILE>) {
 	print OUTFILE "CellHeader=X\tY\tPROBE\tFEAT\tQUAL\tEXPOS\tPOS\tCBASE\tPBASE\tTBASE\tATOM\tINDEX\tCODONIND\tCODON\tREGIONTYPE\tREGION\n";
 	for($i=0; $i<$numprobes; $i++) {
 
-	    @c = split(/[\.\|]/, $probes[$i]);
+            my ($x, $y) = $probes[$i] =~ /^(\d+)[\|\.](\d+)$/;
+#	    @c = split(/[\.\|]/, $probes[$i]);
 #            print STDERR "probes[$i] = $probes[$i]\n";
-#             print STDERR "c[0] = $c[0]\n";
+
 	    $p = $pbase{$probes[$i]};
 	    $t = $tbase{$probes[$i]};
 	    $ii = $i+1;
-	    print OUTFILE "Cell$ii=$c[0]\t$c[1]\tN\tx\t$a[0]\t$ii\t13\tx\t$p\t$t\tx\tx\t-1\t-1\t99\t \n";
+	    print OUTFILE "Cell$ii=$x\t$y\tN\tx\t$a[0]\t$ii\t13\tx\t$p\t$t\tx\tx\t-1\t-1\t99\t \n";
 	}
 	print OUTFILE "\n";
 	$unit++;
