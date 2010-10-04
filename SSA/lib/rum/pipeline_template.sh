70a2e942f9291d38bd89725141ec8fcb11e44347
Greg Grant <ggrant@pcbi.upenn.edu>
2010-10-04 11:43:28 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@37112 735e2a04-f8fc-0310-8a1b-f2942603c481

--- SSA/lib/rum/pipeline_template.sh
+++ SSA/lib/rum/pipeline_template.sh
@@ -4,32 +4,25 @@
 
 # genome bowtie starts here.  Remove from xxx0 to xxx2 for blat only mapping
 
-echo "starting..." > OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "starting..." `date` `date +%s` > OUTDIR/rum.log_chunk.CHUNK
 BOWTIEEXE -a --best --strata -f GENOMEBOWTIE READSFILE.CHUNK -v 3 --suppress 6,7,8 -p 1 > OUTDIR/X.CHUNK
-echo "finished first bowtie run" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished first bowtie run" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 perl SCRIPTSDIR/make_GU_and_GNU.pl OUTDIR/X.CHUNK OUTDIR/GU.CHUNK OUTDIR/GNU.CHUNK PAIREDEND
-echo "finished parsing genome bowtie run" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished parsing genome bowtie run" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 
 # xxx1
 
 # transcriptome bowtie starts here.  Remove from xxx1 to xxx2 for dna mapping
 
 BOWTIEEXE -a --best --strata -f TRANSCRIPTOMEBOWTIE READSFILE.CHUNK -v 3 --suppress 6,7,8 -p 1 > OUTDIR/Y.CHUNK
-echo "finished second bowtie run" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished second bowtie run" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 perl SCRIPTSDIR/make_TU_and_TNU.pl OUTDIR/Y.CHUNK GENEANNOTFILE OUTDIR/TU.CHUNK OUTDIR/TNU.CHUNK PAIREDEND
-echo "finished parsing transcriptome bowtie run" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished parsing transcriptome bowtie run" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 
 perl SCRIPTSDIR/merge_GU_and_TU.pl OUTDIR/GU.CHUNK OUTDIR/TU.CHUNK OUTDIR/GNU.CHUNK OUTDIR/TNU.CHUNK OUTDIR/BowtieUnique.CHUNK OUTDIR/CNU.CHUNK PAIREDEND -readlength READLENGTH
-echo "finished merging TU and GU" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished merging TU and GU" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 perl SCRIPTSDIR/merge_GNU_and_TNU_and_CNU.pl OUTDIR/GNU.CHUNK OUTDIR/TNU.CHUNK OUTDIR/CNU.CHUNK OUTDIR/BowtieNU.CHUNK
-echo "finished merging GNU, TNU and CNU" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished merging GNU, TNU and CNU" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 
 # xxx2
 
@@ -39,37 +32,28 @@ echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
 # cp OUTDIR/GNU.CHUNK OUTDIR/BowtieNU.CHUNK
 
 perl SCRIPTSDIR/make_unmapped_file.pl READSFILE.CHUNK OUTDIR/BowtieUnique.CHUNK OUTDIR/BowtieNU.CHUNK OUTDIR/R.CHUNK PAIREDEND
-echo "finished making R" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished making R" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 
 BLATEXE GENOMEBLAT OUTDIR/R.CHUNK OUTDIR/R.CHUNK.blat -minScore=20 -minIdentity=MINIDENTITY SPEED
-echo "finished first BLAT run" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished first BLAT run" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 MDUSTEXE OUTDIR/R.CHUNK > OUTDIR/R.mdust.CHUNK
-echo "finished running mdust on R" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished running mdust on R" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 perl SCRIPTSDIR/parse_blat_out.pl OUTDIR/R.CHUNK OUTDIR/R.CHUNK.blat OUTDIR/R.mdust.CHUNK OUTDIR/BlatUnique.CHUNK OUTDIR/BlatNU.CHUNK MAXINSERTIONSALLOWED
-echo "finished parsing first BLAT run" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished parsing first BLAT run" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 perl SCRIPTSDIR/merge_Bowtie_and_Blat.pl OUTDIR/BowtieUnique.CHUNK OUTDIR/BlatUnique.CHUNK OUTDIR/BowtieNU.CHUNK OUTDIR/BlatNU.CHUNK OUTDIR/RUM_Unique_temp.CHUNK OUTDIR/RUM_NU_temp.CHUNK PAIREDEND -readlength READLENGTH
-echo "finished merging Bowtie and Blat" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished merging Bowtie and Blat" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 perl SCRIPTSDIR/RUM_finalcleanup.pl OUTDIR/RUM_Unique_temp.CHUNK OUTDIR/RUM_NU_temp.CHUNK OUTDIR/RUM_Unique_temp2.CHUNK OUTDIR/RUM_NU_temp2.CHUNK GENOMEFA OUTDIR/sam_header.CHUNK -faok COUNTMISMATCHES
-echo "finished cleaning up final results" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
-perl SCRIPTSDIR/sort_RUM.pl OUTDIR/RUM_Unique_temp2.CHUNK OUTDIR/RUM_Unique.CHUNK
+echo "finished cleaning up final results" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
+perl SCRIPTSDIR/sort_RUM_by_id.pl OUTDIR/RUM_Unique_temp2.CHUNK OUTDIR/RUM_Unique.CHUNK
 perl SCRIPTSDIR/limit_NU.pl OUTDIR/RUM_NU_temp2.CHUNK LIMITNUCUTOFF > OUTDIR/RUM_NU_temp3.CHUNK
 # the following is correct, temp2 gets replaced with temp3 by RUM_runner.pl if limit_NU option is used
-perl SCRIPTSDIR/sort_RUM.pl OUTDIR/RUM_NU_temp2.CHUNK OUTDIR/RUM_NU.CHUNK
-echo "finished sorting final results" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+perl SCRIPTSDIR/sort_RUM_by_id.pl OUTDIR/RUM_NU_temp2.CHUNK OUTDIR/RUM_NU.CHUNK
+echo "finished sorting final results" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 rm OUTDIR/RUM_Unique_temp.CHUNK
 rm OUTDIR/RUM_NU_temp.CHUNK
 rm OUTDIR/RUM_Unique_temp2.CHUNK
 rm OUTDIR/RUM_NU_temp2.CHUNK
 perl SCRIPTSDIR/rum2sam.pl OUTDIR/RUM_Unique.CHUNK OUTDIR/RUM_NU.CHUNK READSFILE.CHUNK QUALSFILE.CHUNK OUTDIR/RUM.sam.CHUNK
-echo "finished converting to SAM" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "finished converting to SAM" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
 
-echo "pipeline complete" >> OUTDIR/rum_log.CHUNK
-echo `date` `date +%s` >> OUTDIR/rum_log.CHUNK
+echo "pipeline complete" `date` `date +%s` >> OUTDIR/rum.log_chunk.CHUNK
