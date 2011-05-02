7da0ab0fe8c0c5909e53bcd0789b296c89767dac
Greg Grant <ggrant@pcbi.upenn.edu>
2011-05-02 00:23:03 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GGTools/trunk@40504 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ SSA/bin/spans2spans.pl
@@ -0,0 +1,599 @@
+#!/usr/bin/perl
+
+# Written by Gregory R. Grant
+# University of Pennsylvania, 2010
+
+if(@ARGV < 1) {
+    die "
+Usage: spans2spans.pl <file1> <chr col> <start col> <end col> <file2> <chr col> <start col> <end col> <value col> <mode> [options]
+
+Where:
+
+<file1> and <file2> should have the three columns: chr, start, end
+whose column numbers are specified by <chr col>, <start col> and
+<end col> (start counting at 1).  
+
+<mode>:
+   'max' for max
+   others will be implemented...
+
+options:
+           -s1 n  : skip n lines at the beginning of file 1
+           -s2 n  : skip n lines at the beginning of file 2
+           -id n  : column n has ids that you want reported in the output file
+
+What it does:
+------------
+For each span in file1, it finds the spans in file2 that overlap it and
+computes the value among those spans of the value in column <value col>,
+according to <mode>.  Currently only <mode>=max is implemented.
+
+";
+}
+
+$file1 = $ARGV[0];
+$chrcol1 = $ARGV[1]-1;
+$startcol1 = $ARGV[2]-1;
+$endcol1 = $ARGV[3]-1;
+$file2 = $ARGV[4];
+$chrcol2 = $ARGV[5]-1;
+$startcol2 = $ARGV[6]-1;
+$endcol2 = $ARGV[7]-1;
+$value_col=$ARGV[8]-1;
+$mode = $ARGV[9];
+
+$options_start = @ARGV;
+for($i=@ARGV-1; $i>=0; $i--) {
+    if($ARGV[$i] =~ /^-/) {
+	$options_start = $i;
+    }
+}
+
+$skip1=0;
+$skip2=0;
+$idcol = "-1";
+for($i=$options_start; $i<@ARGV; $i++) {
+    $option_recognized=0;
+    if($ARGV[$i] eq '-s1') {
+	$skip1 = $ARGV[$i+1];
+	$i++;
+	$option_recognized=1;
+    }
+    if($ARGV[$i] eq '-s2') {
+	$skip2 = $ARGV[$i+1];
+	$i++;
+	$option_recognized=1;
+    }
+    if($ARGV[$i] eq '-id') {
+	$idcol = $ARGV[$i+1]-1;
+	$i++;
+	$option_recognized=1;
+    }
+    if($option_recognized==0) {
+	die "\nError: option '$ARGV[$i]' not recognized.\n\n";
+    }
+}
+
+open(INFILE1, $file1);
+for($i=0; $i<$skip1; $i++) {
+    $line = <INFILE1>;
+}
+open(INFILE2, $file2);
+for($i=0; $i<$skip2; $i++) {
+    $line = <INFILE2>;
+}
+
+$line2 = <INFILE2>;
+chomp($line2);
+@a = split(/\t/,$line2);
+$chr2 = $a[$chrcol2];
+$start2 = $a[$startcol2];
+$end2 = $a[$endcol2];
+$value = $a[$value_col];
+$span2 = "$chr2:$start2-$end2";
+$current_spans{$span2}=1;
+
+$CNT=0;
+while($line1 = <INFILE1>) {
+    chomp($line1);
+    @a = split(/\t/,$line1);
+    $chr1 = $a[$chrcol1];
+    $start1 = $a[$startcol1];
+    $end1 = $a[$endcol1];
+    $span1 = "$chr1:$start1-$end1";
+    if($idcol >= 0) {
+	$id{$span1} = $a[$idcol];
+    }
+    $VAL{$span1}="NA";
+#    print "--------\nspan1 = $span1\n";
+    $iter{$span1} = $CNT;
+    $CNT++;
+    $flag = 0;
+    until(($chr1 eq $chr2 && $end1<$start2) || &cmpChrs2($chr1,$chr2)>0 || $flag==1) {
+	$line2 = <INFILE2>;
+	chomp($line2);
+	if($line2 eq '') {
+	    $flag = 1;
+	}
+	@a = split(/\t/,$line2);
+	$chr2 = $a[$chrcol2];
+	$start2 = $a[$startcol2];
+	$end2 = $a[$endcol2];
+	$value = $a[$value_col];
+	$span2 = "$chr2:$start2-$end2";
+	$current_spans{$span2}=$value;
+    }
+    foreach $span (keys %current_spans) {
+	$span =~ /^(.*):(\d+)-(\d+)/;
+	$chr = $1;
+	$start = $2;
+	$end = $3;
+	$value = $current_spans{$span};
+	if($chr == $chr1 && $end1 >= $start && $start1 <= $end) {
+	    if($mode eq "max") {
+		if(defined $VAL{$span1} && $VAL{$span1} ne "NA") {
+		    if($VAL{$span1}<$value) {
+			$VAL{$span1} = $value;
+		    }
+		} else {
+		    $VAL{$span1} = $value;
+		}
+	    }
+	}
+	if(($chr == $chr1 && $start1 > $end) || &cmpChrs2($chr,$chr1) < 0) {
+	    delete $current_spans{$span};
+	}
+    }
+}
+close(INFILE);
+
+foreach $key (sort {$iter{$a}<=>$iter{$b}} keys %VAL) {
+    print "$key\t$VAL{$key}";
+    if($idcol>=0) {
+	print "\t$id{$key}";
+    }
+    print "\n";
+}
+
+sub cmpChrs2 () {
+    ($a2_c, $b2_c) = @_;
+    $a2_c = lc($a2_c);
+    $b2_c = lc($b2_c);
+    if($a2_c eq $b2_c) {
+	return "0";
+    }
+    if($a2_c eq 'finished1234') {
+	return -1;
+    }
+    if($b2_c eq 'finished1234') {
+	return 1;
+    }
+    if($a2_c =~ /^\d+$/ && !($b2_c =~ /^\d+$/)) {
+        return 1;
+    }
+    if($b2_c =~ /^\d+$/ && !($a2_c =~ /^\d+$/)) {
+        return -1;
+    }
+    if($a2_c =~ /^[ivxym]+$/ && !($b2_c =~ /^[ivxym]+$/)) {
+        return 1;
+    }
+    if($b2_c =~ /^[ivxym]+$/ && !($a2_c =~ /^[ivxym]+$/)) {
+        return -1;
+    }
+    if($a2_c eq 'm' && ($b2_c eq 'y' || $b2_c eq 'x')) {
+        return -1;
+    }
+    if($b2_c eq 'm' && ($a2_c eq 'y' || $a2_c eq 'x')) {
+        return 1;
+    }
+    if($a2_c =~ /^[ivx]+$/ && $b2_c =~ /^[ivx]+$/) {
+        $a2_c = "chr" . $a2_c;
+        $b2_c = "chr" . $b2_c;
+    }
+    if($a2_c =~ /$b2_c/) {
+	return -1;
+    }
+    if($b2_c =~ /$a2_c/) {
+	return 1;
+    }
+    # dealing with roman numerals starts here
+    if($a2_c =~ /chr([ivx]+)/ && $b2_c =~ /chr([ivx]+)/) {
+	$a2_c =~ /chr([ivx]+)/;
+	$a2_roman = $1;
+	$b2_c =~ /chr([ivx]+)/;
+	$b2_roman = $1;
+	$a2_arabic = arabic($a2_roman);
+    	$b2_arabic = arabic($b2_roman);
+	if($a2_arabic > $b2_arabic) {
+	    return -1;
+	} 
+	if($a2_arabic < $b2_arabic) {
+	    return 1;
+	}
+	if($a2_arabic == $b2_arabic) {
+	    $tempa = $a2_c;
+	    $tempb = $b2_c;
+	    $tempa =~ s/chr([ivx]+)//;
+	    $tempb =~ s/chr([ivx]+)//;
+	    undef %temphash;
+	    $temphash{$tempa}=1;
+	    $temphash{$tempb}=1;
+	    foreach $tempkey (sort {cmpChrs($a,$b)} keys %temphash) {
+		if($tempkey eq $tempa) {
+		    return 1;
+		} else {
+		    return -1;
+		}
+	    }
+	}
+    }
+    if($b2_c =~ /chr([ivx]+)/ && !($a2_c =~ /chr([a-z]+)/) && !($a2_c =~ /chr(\d+)/)) {
+	return -1;
+    }
+    if($a2_c =~ /chr([ivx]+)/ && !($b2_c =~ /chr([a-z]+)/) && !($b2_c =~ /chr(\d+)/)) {
+	return 1;
+    }
+    # roman numerals ends here
+    if($a2_c =~ /chr(\d+)$/ && $b2_c =~ /chr.*_/) {
+        return 1;
+    }
+    if($b2_c =~ /chr(\d+)$/ && $a2_c =~ /chr.*_/) {
+        return -1;
+    }
+    if($a2_c =~ /chr([a-z])$/ && $b2_c =~ /chr.*_/) {
+        return 1;
+    }
+    if($b2_c =~ /chr([a-z])$/ && $a2_c =~ /chr.*_/) {
+        return -1;
+    }
+    if($a2_c =~ /chr(\d+)/) {
+        $numa = $1;
+        if($b2_c =~ /chr(\d+)/) {
+            $numb = $1;
+            if($numa < $numb) {return 1;}
+	    if($numa > $numb) {return -1;}
+	    if($numa == $numb) {
+		$tempa = $a2_c;
+		$tempb = $b2_c;
+		$tempa =~ s/chr\d+//;
+		$tempb =~ s/chr\d+//;
+		undef %temphash;
+		$temphash{$tempa}=1;
+		$temphash{$tempb}=1;
+		foreach $tempkey (sort {cmpChrs($a,$b)} keys %temphash) {
+		    if($tempkey eq $tempa) {
+			return 1;
+		    } else {
+			return -1;
+		    }
+		}
+	    }
+        } else {
+            return 1;
+        }
+    }
+    if($a2_c =~ /chrx(.*)/ && ($b2_c =~ /chr(y|m)$1/)) {
+	return 1;
+    }
+    if($b2_c =~ /chrx(.*)/ && ($a2_c =~ /chr(y|m)$1/)) {
+	return -1;
+    }
+    if($a2_c =~ /chry(.*)/ && ($b2_c =~ /chrm$1/)) {
+	return 1;
+    }
+    if($b2_c =~ /chry(.*)/ && ($a2_c =~ /chrm$1/)) {
+	return -1;
+    }
+    if($a2_c =~ /chr\d/ && !($b2_c =~ /chr[^\d]/)) {
+	return 1;
+    }
+    if($b2_c =~ /chr\d/ && !($a2_c =~ /chr[^\d]/)) {
+	return -1;
+    }
+    if($a2_c =~ /chr[^xy\d]/ && (($b2_c =~ /chrx/) || ($b2_c =~ /chry/))) {
+        return -1;
+    }
+    if($b2_c =~ /chr[^xy\d]/ && (($a2_c =~ /chrx/) || ($a2_c =~ /chry/))) {
+        return 1;
+    }
+    if($a2_c =~ /chr(\d+)/ && !($b2_c =~ /chr(\d+)/)) {
+        return 1;
+    }
+    if($b2_c =~ /chr(\d+)/ && !($a2_c =~ /chr(\d+)/)) {
+        return -1;
+    }
+    if($a2_c =~ /chr([a-z])/ && !($b2_c =~ /chr(\d+)/) && !($b2_c =~ /chr[a-z]+/)) {
+        return 1;
+    }
+    if($b2_c =~ /chr([a-z])/ && !($a2_c =~ /chr(\d+)/) && !($a2_c =~ /chr[a-z]+/)) {
+        return -1;
+    }
+    if($a2_c =~ /chr([a-z]+)/) {
+        $letter_a = $1;
+        if($b2_c =~ /chr([a-z]+)/) {
+            $letter_b = $1;
+            if($letter_a lt $letter_b) {return 1;}
+	    if($letter_a gt $letter_b) {return -1;}
+        } else {
+            return -1;
+        }
+    }
+    $flag_c = 0;
+    while($flag_c == 0) {
+        $flag_c = 1;
+        if($a2_c =~ /^([^\d]*)(\d+)/) {
+            $stem1_c = $1;
+            $num1_c = $2;
+            if($b2_c =~ /^([^\d]*)(\d+)/) {
+                $stem2_c = $1;
+                $num2_c = $2;
+                if($stem1_c eq $stem2_c && $num1_c < $num2_c) {
+                    return 1;
+                }
+                if($stem1_c eq $stem2_c && $num1_c > $num2_c) {
+                    return -1;
+                }
+                if($stem1_c eq $stem2_c && $num1_c == $num2_c) {
+                    $a2_c =~ s/^$stem1_c$num1_c//;
+                    $b2_c =~ s/^$stem2_c$num2_c//;
+                    $flag_c = 0;
+                }
+            }
+        }
+    }
+    if($a2_c le $b2_c) {
+	return 1;
+    }
+    if($b2_c le $a2_c) {
+	return -1;
+    }
+
+
+    return 1;
+}
+
+sub isroman($) {
+    $arg = shift;
+    $arg ne '' and
+      $arg =~ /^(?: M{0,3})
+                (?: D?C{0,3} | C[DM])
+                (?: L?X{0,3} | X[LC])
+                (?: V?I{0,3} | I[VX])$/ix;
+}
+
+sub arabic($) {
+    $arg = shift;
+    %roman2arabic = qw(I 1 V 5 X 10 L 50 C 100 D 500 M 1000);
+    %roman_digit = qw(1 IV 10 XL 100 CD 1000 MMMMMM);
+    @figure = reverse sort keys %roman_digit;
+    $roman_digit{$_} = [split(//, $roman_digit{$_}, 2)] foreach @figure;
+    isroman $arg or return undef;
+    ($last_digit) = 1000;
+    $arabic = 0;
+    ($arabic);
+    foreach (split(//, uc $arg)) {
+        ($digit) = $roman2arabic{$_};
+        $arabic -= 2 * $last_digit if $last_digit < $digit;
+        $arabic += ($last_digit = $digit);
+    }
+    $arabic;
+}
+
+sub Roman($) {
+    $arg = shift;
+    %roman2arabic = qw(I 1 V 5 X 10 L 50 C 100 D 500 M 1000);
+    %roman_digit = qw(1 IV 10 XL 100 CD 1000 MMMMMM);
+    @figure = reverse sort keys %roman_digit;
+    $roman_digit{$_} = [split(//, $roman_digit{$_}, 2)] foreach @figure;
+    0 < $arg and $arg < 4000 or return undef;
+    $roman = "";
+    ($x, $roman);
+    foreach (@figure) {
+        ($digit, $i, $v) = (int($arg / $_), @{$roman_digit{$_}});
+        if (1 <= $digit and $digit <= 3) {
+            $roman .= $i x $digit;
+        } elsif ($digit == 4) {
+            $roman .= "$i$v";
+        } elsif ($digit == 5) {
+            $roman .= $v;
+        } elsif (6 <= $digit and $digit <= 8) {
+            $roman .= $v . $i x ($digit - 5);
+        } elsif ($digit == 9) {
+            $roman .= "$i$x";
+        }
+        $arg -= $digit * $_;
+        $x = $i;
+    }
+    $roman;
+}
+
+sub roman($) {
+    lc Roman shift;
+}
+
+sub cmpChrs () {
+    $a2_c = lc($b);
+    $b2_c = lc($a);
+    if($a2_c =~ /^\d+$/ && !($b2_c =~ /^\d+$/)) {
+        return 1;
+    }
+    if($b2_c =~ /^\d+$/ && !($a2_c =~ /^\d+$/)) {
+        return -1;
+    }
+    if($a2_c =~ /^[ivxym]+$/ && !($b2_c =~ /^[ivxym]+$/)) {
+        return 1;
+    }
+    if($b2_c =~ /^[ivxym]+$/ && !($a2_c =~ /^[ivxym]+$/)) {
+        return -1;
+    }
+    if($a2_c eq 'm' && ($b2_c eq 'y' || $b2_c eq 'x')) {
+        return -1;
+    }
+    if($b2_c eq 'm' && ($a2_c eq 'y' || $a2_c eq 'x')) {
+        return 1;
+    }
+    if($a2_c =~ /^[ivx]+$/ && $b2_c =~ /^[ivx]+$/) {
+        $a2_c = "chr" . $a2_c;
+        $b2_c = "chr" . $b2_c;
+    }
+    if($a2_c =~ /$b2_c/) {
+	return -1;
+    }
+    if($b2_c =~ /$a2_c/) {
+	return 1;
+    }
+    # dealing with roman numerals starts here
+    if($a2_c =~ /chr([ivx]+)/ && $b2_c =~ /chr([ivx]+)/) {
+	$a2_c =~ /chr([ivx]+)/;
+	$a2_roman = $1;
+	$b2_c =~ /chr([ivx]+)/;
+	$b2_roman = $1;
+	$a2_arabic = arabic($a2_roman);
+    	$b2_arabic = arabic($b2_roman);
+	if($a2_arabic > $b2_arabic) {
+	    return -1;
+	} 
+	if($a2_arabic < $b2_arabic) {
+	    return 1;
+	}
+	if($a2_arabic == $b2_arabic) {
+	    $tempa = $a2_c;
+	    $tempb = $b2_c;
+	    $tempa =~ s/chr([ivx]+)//;
+	    $tempb =~ s/chr([ivx]+)//;
+	    undef %temphash;
+	    $temphash{$tempa}=1;
+	    $temphash{$tempb}=1;
+	    foreach $tempkey (sort {cmpChrs($a,$b)} keys %temphash) {
+		if($tempkey eq $tempa) {
+		    return 1;
+		} else {
+		    return -1;
+		}
+	    }
+	}
+    }
+    if($b2_c =~ /chr([ivx]+)/ && !($a2_c =~ /chr([a-z]+)/) && !($a2_c =~ /chr(\d+)/)) {
+	return -1;
+    }
+    if($a2_c =~ /chr([ivx]+)/ && !($b2_c =~ /chr([a-z]+)/) && !($b2_c =~ /chr(\d+)/)) {
+	return 1;
+    }
+
+    # roman numerals ends here
+    if($a2_c =~ /chr(\d+)$/ && $b2_c =~ /chr.*_/) {
+        return 1;
+    }
+    if($b2_c =~ /chr(\d+)$/ && $a2_c =~ /chr.*_/) {
+        return -1;
+    }
+    if($a2_c =~ /chr([a-z])$/ && $b2_c =~ /chr.*_/) {
+        return 1;
+    }
+    if($b2_c =~ /chr([a-z])$/ && $a2_c =~ /chr.*_/) {
+        return -1;
+    }
+    if($a2_c =~ /chr(\d+)/) {
+        $numa = $1;
+        if($b2_c =~ /chr(\d+)/) {
+            $numb = $1;
+            if($numa < $numb) {return 1;}
+	    if($numa > $numb) {return -1;}
+	    if($numa == $numb) {
+		$tempa = $a2_c;
+		$tempb = $b2_c;
+		$tempa =~ s/chr\d+//;
+		$tempb =~ s/chr\d+//;
+		undef %temphash;
+		$temphash{$tempa}=1;
+		$temphash{$tempb}=1;
+		foreach $tempkey (sort {cmpChrs($a,$b)} keys %temphash) {
+		    if($tempkey eq $tempa) {
+			return 1;
+		    } else {
+			return -1;
+		    }
+		}
+	    }
+        } else {
+            return 1;
+        }
+    }
+    if($a2_c =~ /chrx(.*)/ && ($b2_c =~ /chr(y|m)$1/)) {
+	return 1;
+    }
+    if($b2_c =~ /chrx(.*)/ && ($a2_c =~ /chr(y|m)$1/)) {
+	return -1;
+    }
+    if($a2_c =~ /chry(.*)/ && ($b2_c =~ /chrm$1/)) {
+	return 1;
+    }
+    if($b2_c =~ /chry(.*)/ && ($a2_c =~ /chrm$1/)) {
+	return -1;
+    }
+    if($a2_c =~ /chr\d/ && !($b2_c =~ /chr[^\d]/)) {
+	return 1;
+    }
+    if($b2_c =~ /chr\d/ && !($a2_c =~ /chr[^\d]/)) {
+	return -1;
+    }
+    if($a2_c =~ /chr[^xy\d]/ && (($b2_c =~ /chrx/) || ($b2_c =~ /chry/))) {
+        return -1;
+    }
+    if($b2_c =~ /chr[^xy\d]/ && (($a2_c =~ /chrx/) || ($a2_c =~ /chry/))) {
+        return 1;
+    }
+    if($a2_c =~ /chr(\d+)/ && !($b2_c =~ /chr(\d+)/)) {
+        return 1;
+    }
+    if($b2_c =~ /chr(\d+)/ && !($a2_c =~ /chr(\d+)/)) {
+        return -1;
+    }
+    if($a2_c =~ /chr([a-z])/ && !($b2_c =~ /chr(\d+)/) && !($b2_c =~ /chr[a-z]+/)) {
+        return 1;
+    }
+    if($b2_c =~ /chr([a-z])/ && !($a2_c =~ /chr(\d+)/) && !($a2_c =~ /chr[a-z]+/)) {
+        return -1;
+    }
+    if($a2_c =~ /chr([a-z]+)/) {
+        $letter_a = $1;
+        if($b2_c =~ /chr([a-z]+)/) {
+            $letter_b = $1;
+            if($letter_a lt $letter_b) {return 1;}
+	    if($letter_a gt $letter_b) {return -1;}
+        } else {
+            return -1;
+        }
+    }
+    $flag_c = 0;
+    while($flag_c == 0) {
+        $flag_c = 1;
+        if($a2_c =~ /^([^\d]*)(\d+)/) {
+            $stem1_c = $1;
+            $num1_c = $2;
+            if($b2_c =~ /^([^\d]*)(\d+)/) {
+                $stem2_c = $1;
+                $num2_c = $2;
+                if($stem1_c eq $stem2_c && $num1_c < $num2_c) {
+                    return 1;
+                }
+                if($stem1_c eq $stem2_c && $num1_c > $num2_c) {
+                    return -1;
+                }
+                if($stem1_c eq $stem2_c && $num1_c == $num2_c) {
+                    $a2_c =~ s/^$stem1_c$num1_c//;
+                    $b2_c =~ s/^$stem2_c$num2_c//;
+                    $flag_c = 0;
+                }
+            }
+        }
+    }
+    if($a2_c le $b2_c) {
+	return 1;
+    }
+    if($b2_c le $a2_c) {
+	return -1;
+    }
+
+    return 1;
+}
